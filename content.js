eb72b20c953152be5be5bb6d1f1428e92335687a
Sean <e0406739@u.nus.edu>
2022-04-17 14:51:28 +0800

    update video size

--- content.js
+++ content.js
@@ -1,14 +1,21 @@
 function removeChat() {
   var node = document.getElementsByClassName("transcript-wrapper");
-  if (node.length > 0) {
+  if (node?.length > 0) {
     console.log("came here1");
     node[0].remove();
   }
 
   var expandNode = document.getElementsByClassName("player-view-wrapper");
-  if (expandNode.length > 0) {
+  if (expandNode?.length > 0) {
     console.log("came here2");
     expandNode[0].style.width = "calc(100% - 20px)";
+  }
+
+  var playerViewNode = document.getElementsByClassName("player-view");
+  if (playerViewNode?.length > 0) {
+    console.log("came here3");
+    playerViewNode[0].style.width = "100%";
+    playerViewNode[0].style.height = "100%";
   }
 }
 
